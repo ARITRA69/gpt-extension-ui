15a5e34e3bd14838717aed0aa9d70978a11b1d9f
Aritra Sarkar <aritrasarkar2002@gmail.com>
2023-11-10 10:52:48 +0530

    testing

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,79 @@
+"use client"
+
+import { useCompletion } from "ai/react"
 import { useState } from "react"
 
+import "./style.css"
+
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [input, setInput] = useState("")
+  const [result, setResult] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true)
+      const data = await fetch("http://localhost:3000/api/generate-text", {
+        method: "POST",
+        body: JSON.stringify({ input })
+      })
+      const jsonResponse = await data.json()
+      setResult(jsonResponse.data)
+      console.log("json response--->", jsonResponse.data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  // const {
+  //   completion,
+  //   input,
+  //   stop,
+  //   isLoading,
+  //   handleInputChange,
+  //   handleSubmit
+  // } = useCompletion({ api: "http://localhost:3000/api/generate-text" })
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="w-[500px] h-[500px] bg-black flex flex-col justify-between items-center py-10">
+      <h1 className="bg-gradient-to-tr from-rose-500 via-fuchsia-500 to-violet-500 bg-clip-text text-transparent text-5xl font-bold">
+        Curiodom
+      </h1>
+      <div className="pt-10 justify-between space-y-4">
+        {isLoading ? (
+          <div className="text-center text-white text-xl">
+            <img
+              src="https://media.tenor.com/RVvnVPK-6dcAAAAC/reload-cat.gif"
+              className="w-[80px] h-[80px]"
+            />
+          </div>
+        ) : (
+          <output className="w-full h-[250px] rounded-xl text-xl bg-transparent text-white">
+            {/* {completion} */}
+            {result}
+          </output>
+        )}
+        <div className="flex space-x-4 h-[50px]">
+          {/* <form onSubmit={handleSubmit} className="flex space-x-4 h-[50px]"> */}
+          <input
+            type="text"
+            placeholder="type here.."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            // onChange={handleInputChange}
+            className="w-full text-xl rounded-xl"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white text-xl px-4 py-2 rounded-xl"
+            onClick={handleClick}>
+            Generate
+          </button>
+          {/* </form> */}
+        </div>
+      </div>
     </div>
   )
 }
